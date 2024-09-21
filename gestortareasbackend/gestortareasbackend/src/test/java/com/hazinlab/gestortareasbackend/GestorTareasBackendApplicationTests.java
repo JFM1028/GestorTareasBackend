0d69d9f692c0195a6f262327b569c8875741eaca
Diego <58009401+marzo245@users.noreply.github.com>
2024-09-21 14:47:05 -0500

    Configuracion proyecton en spring boot

---
+++ gestortareasbackend/gestortareasbackend/src/test/java/com/hazinlab/gestortareasbackend/GestorTareasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazinlab.gestortareasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorTareasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
